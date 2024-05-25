c56b588730a45bb2c4c673e0e85ec3e2173ad30a
Evangelos Kostopoulos <kostopoulos.e@gmail.com>
2024-05-25 16:03:41 +0100

    add sumAll logic

--- arrays_and_slices/sum.go
+++ arrays_and_slices/sum.go
@@ -5,5 +5,15 @@ func Sum(numbers []int) int {
 	for _, number := range numbers {
 		sum += number
 	}
+
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
 }
