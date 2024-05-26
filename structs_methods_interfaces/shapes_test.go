dac6b8f019c7e51f3868e95dd6802b5a3e9e31f2
Evangelos Kostopoulos <kostopoulos.e@gmail.com>
2024-05-26 10:24:58 +0100

    add rectangle struct to shapes

--- structs_methods_interfaces/shapes_test.go
+++ structs_methods_interfaces/shapes_test.go
@@ -3,7 +3,7 @@ package structsmethodsinterfaces
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	got := Perimeter(Rectangle{10.0, 10.0})
 	want := 40.0
 
 	if got != want {
@@ -12,7 +12,7 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(12.0, 6.0)
+	got := Area(Rectangle{12.0, 6.0})
 	want := 72.0
 
 	if got != want {
