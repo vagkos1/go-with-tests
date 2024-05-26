6c2171ede0ca1b98afa90ce69ab950f1401b0e31
Evangelos Kostopoulos <kostopoulos.e@gmail.com>
2024-05-26 13:09:00 +0100

    introduce BTC from int

--- pointers_errors/wallet_test.go
+++ pointers_errors/wallet_test.go
@@ -7,12 +7,12 @@ import (
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
 
-	wallet.Deposit(10)
+	wallet.Deposit(Bitcoin(10))
 
 	got := wallet.Balance()
-	want := 10
+	want := Bitcoin(10)
 
 	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		t.Errorf("got %s want %s", got, want)
 	}
 }
