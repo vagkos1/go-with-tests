69202fc3851f8eaa7a4c5d6a45590f82744763d2
Evangelos Kostopoulos <kostopoulos.e@gmail.com>
2024-05-26 13:23:40 +0100

    expect proper error when withdrawing insufficient funds

--- pointers_errors/wallet_test.go
+++ pointers_errors/wallet_test.go
@@ -13,15 +13,32 @@ func TestWallet(t *testing.T) {
 			t.Errorf("got %d want %d", got, want)
 		}
 	}
+	assertError := func(t testing.TB, got error, want string) {
+		t.Helper()
+		if got == nil {
+			t.Fatal("wanted an error but did not get one")
+		}
+		if got.Error() != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	}
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
 		assertBalance(t, wallet, Bitcoin(10))
 	})
-	t.Run("withdraw", func(t *testing.T) {
+	t.Run("withdraw OK", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
 		wallet.Withdraw(Bitcoin(4))
 		assertBalance(t, wallet, Bitcoin(6))
+	})
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+
+		err := wallet.Withdraw(21)
+		assertError(t, err, "cannot withraw, insufficient funds")
+		assertBalance(t, wallet, Bitcoin(startingBalance))
 	})
 }
