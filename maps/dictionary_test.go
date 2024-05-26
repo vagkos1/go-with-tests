d27f13f64403bdfa98a6090cdc4b58bbac8a675f
Evangelos Kostopoulos <kostopoulos.e@gmail.com>
2024-05-26 14:07:21 +0100

    add to dict

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -34,3 +34,23 @@ func assertError(t testing.TB, got, want error) {
 		t.Errorf("got error %q want %q", got, want)
 	}
 }
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	word := "test"
+	definition := "this is just a test"
+
+	dictionary.Add(word, definition)
+
+	assertDefinition(t, dictionary, word, definition)
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+	assertStrings(t, got, definition)
+}
